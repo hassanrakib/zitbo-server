86c060431c4eac38a29d8fb62d5053b93625bc75
rakibhassan2 <mailrakibhassan@gmail.com>
2023-06-13 17:23:15 +0600

    socket initialized

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+// require express to create an express app
 const express = require("express");
+// require http module to create http server
+const http = require("http");
+// get Server class from socket.io to create socket.io server
+const { Server } = require("socket.io");
+// mongodb db
 const { MongoClient, ServerApiVersion } = require("mongodb");
+// allow cross origin resource sharing
 const cors = require("cors");
+// jsonwebtoken package for jwt token implementation
 const jwt = require("jsonwebtoken");
+// dot env package for .env file usage
 require("dotenv").config();
+
+// express app
+const app = express();
+
+// http server
+const server = http.createServer(app);
+
+// create socket.io server
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+// PORT to run the server
 const port = process.env.PORT || 5000;
 
-const app = express();
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@zitbo-1.8pinoyh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -25,21 +48,21 @@ const verifyJWT = (req, res, next) => {
   const authorizationHeader = req.headers?.authorization;
 
   // if no authorization header then the request is unauthorized
-  if(!authorizationHeader) return res.status(401).send({message: 'Unauthorized Access'});
+  if (!authorizationHeader)
+    return res.status(401).send({ message: "Unauthorized Access" });
 
   const token = authorizationHeader.split(" ")[1];
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     // if err in verifying the token
-    if (err) return res.status(403).send({message: 'Access Forbidden'});
+    if (err) return res.status(403).send({ message: "Access Forbidden" });
 
     // if successful in verifying the token with the secret key
     req.decoded = decoded;
     // call the next handler of the route
-    next(); 
-  })
-
-}
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -55,7 +78,11 @@ async function run() {
     const db = client.db("zitbo-1_db-1");
     const users = db.collection("users");
 
-    // APIs
+    // ********************
+
+    //  REST APIs that uses req res model
+
+    // ********************
 
     // validate username by checking if username is unique
     app.get("/users/validate/:username", async (req, res) => {
@@ -107,6 +134,18 @@ async function run() {
 
       res.status(403).send({ token: "" });
     });
+
+    // ********************
+
+    //  socket.io server implementation for bidirectional event based realtime communication
+
+    // ********************
+
+    io.on("connection", (socket) => {
+      console.log('New user connected...');
+    })
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -118,6 +157,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to zitbo server!");
 });
 
-app.listen(port, () => {
+// listen to the port from http server
+server.listen(port, () => {
   console.log(`Zitbo server is running in port ${port}`);
 });
