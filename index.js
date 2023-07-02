9112268510dca53d583a0cd21055ed1e35d6197a
rakibhassan3 <mailrakibhassan@gmail.com>
2023-07-02 08:15:20 +0600

    changed the client side address for socket io server

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 // create socket.io server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.LOCAL_HOST,
   },
 });
 
